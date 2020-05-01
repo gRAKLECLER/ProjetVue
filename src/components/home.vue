01c119ab41db295c253ece95ae9cf7b8ac53a9a2
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2020-05-01 19:56:40 +0200

    first commit vueproject

---
+++ src/components/home.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <img class="logo" v-bind:src="img" alt="logo site">
+    <div class="list" v-for="item in items" :key="item.message">
+      <router-link to="/">{{ item.message }}</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "list",
+  data() {
+    return {
+      items: [
+        { message: "acceuil" },
+        { message: "nos articles" },
+        { message: "nos membres" },
+      ],
+      img: "../../assets/logo.png"
+    }
+  }
+};
+</script>
+
+<style>
+
+header{
+  width: 100vw;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.list{
+  list-style: none;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <list/>
+    <h1>Generater of cards</h1>
+  </div>
+</template>
+
+<script>
+import list from "@/components/home.vue";
+
+export default {
+  name: "home",
+  components: {
+    list,
+  }
+};
+</script>
